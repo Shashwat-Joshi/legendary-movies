ee1d60d99fa60948b08acf545b68461df9c6826e
Shashwat Joshi <shashwat2002joshi@gmail.com>
2023-12-14 20:57:52 +0530

    Added WebConfig for filtering Requests and allowing CORS from a particular endpoint

---
+++ src/main/java/com/flixmovies/imdb/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.flixmovies.imdb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5000")
+                        .allowedMethods(
+                                HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.PATCH.name())
+                        .allowedHeaders(HttpHeaders.CONTENT_TYPE);
+            }
+        };
+    }
+}
